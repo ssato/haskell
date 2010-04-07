9f503ea5945d0209403bc4e484b3e08db6c7b18a
Satoru SATOH <satoru.satoh@gmail.com>
2010-04-07 22:34:28 +0900

    updated (simplified) xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,55 +1,108 @@
 --
--- http://www.haskell.org/haskellwiki/Xmonad/Config_archive/Template_xmonad.hs_%280.8%29
+-- xmonad example config file.
+--
+-- A template showing all available configuration hooks,
+-- and how to override the defaults in your own xmonad.hs conf file.
+--
+-- Normally, you'd only override those defaults you care about.
+--
+-- References:
+--  * http://www.haskell.org/haskellwiki/Xmonad/Using_xmonad_in_Gnome
+--  * http://d.hatena.ne.jp/mono-hate/20081210/1228915177
+--
 --
 
---
--- http://www.haskell.org/haskellwiki/Xmonad/Basic_Desktop_Environment_Integration
---
-{-
 import XMonad
-import XMonad.Config.Gnome
- 
-main = xmonad gnomeConfig
--}
+import System.Exit
 
---
--- http://www.haskell.org/haskellwiki/Xmonad/Using_xmonad_in_Gnome
--- http://www.haskell.org/haskellwiki/Xmonad/General_xmonad.hs_config_tips
---
-import XMonad
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.EwmhDesktops
 import XMonad.Actions.CycleWS
-import XMonad.Actions.DynamicWorkspaces
-import qualified XMonad.StackSet as W
+import XMonad.Config.Gnome
+import XMonad.Hooks.ManageHelpers 
 import XMonad.Util.EZConfig
 
+-- import XMonad.Prompt
+-- import XMonad.Prompt.Shell
 
--- myWorkspaces    = ["1","2","3","4","5"]
-myWorkspaces    = ["1:term", "2:web", "3:mail", "4:misc", "5:other"]
 
-myKeys = 
-        [
-        -- moving workspaces
-          ("C-M-<Left>",  prevWS)
-        , ("C-M-<Right>", nextWS)
-        , ("M-S-<Left>",  shiftToPrev )
-        , ("M-S-<Right>", shiftToNext )
+main = xmonad $ gnomeConfig
+    {
+    -- simple stuff
+    terminal           = "gnome-terminal",
+    focusFollowsMouse  = True,
+    borderWidth        = 1,
+    modMask            = myModMask,
+    numlockMask        = myNumlockMask,
+    workspaces         = myWorkspaces,
+    normalBorderColor  = myNormalBorderColor,
+    focusedBorderColor = myFocusedBorderColor,
 
-        -- Make mod+shift+q to quit gnome-session
-        , ("M-S-<Return>", spawn "gnome-terminal")
-
-        -- Make mod+shift+q to quit gnome-session
-        , ("M-S-q", spawn "gnome-session-save --gui --kill")
-
-        -- Lock Screen
-        , ("M-S-l", spawn "gnome-screensaver-command -l")
+    manageHook = composeAll
+        [ manageHook gnomeConfig
+        , isFullscreen --> doFullFloat
         ]
 
+    , logHook    = logHook gnomeConfig
+    }
+    `additionalKeysP` myKeymaps
 
-main = xmonad $ defaultConfig
-                { manageHook = manageDocks <+> manageHook defaultConfig
-                , logHook    = ewmhDesktopsLogHook
-                , workspaces = myWorkspaces
-                , layoutHook = ewmhDesktopsLayout $ avoidStruts $ layoutHook defaultConfig
-                } `additionalKeysP` myKeys
+
+
+-- modMask lets you specify which modkey you want to use. The default
+-- is mod1Mask ("left alt").  You may also consider using mod3Mask
+-- ("right alt"), which does not conflict with emacs keybindings. The
+-- "windows key" is usually mod4Mask.
+--
+myModMask       = mod1Mask
+
+-- The mask for the numlock key. Numlock status is "masked" from the
+-- current modifier status, so the keybindings will work with numlock on or
+-- off. You may need to change this on some systems.
+--
+-- You can find the numlock modifier by running "xmodmap" and looking for a
+-- modifier with Num_Lock bound to it:
+--
+-- > $ xmodmap | grep Num
+-- > mod2        Num_Lock (0x4d)
+--
+-- Set numlockMask = 0 if you don't have a numlock key, or want to treat
+-- numlock status separately.
+--
+myNumlockMask   = mod2Mask
+
+-- The default number of workspaces (virtual screens) and their names.
+-- By default we use numeric strings, but any string may be used as a
+-- workspace name. The number of workspaces is determined by the length
+-- of this list.
+--
+-- A tagging example:
+--
+-- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
+--
+myWorkspaces    = map show [1..5]
+
+-- Border colors for unfocused and focused windows, respectively.
+--
+myNormalBorderColor  = "#dddddd"
+myFocusedBorderColor = "#ff0000"
+
+
+-- Whether focus follows the mouse pointer.
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+
+myKeymaps = [
+            -- ("M-<F2>", shellPrompt defaultXPConfig )
+            ("M-<F2>", gnomeRun)
+
+            , ("M-C-<Left>",    prevWS )
+            , ("M-C-<Right>",   nextWS )
+            , ("M-S-<Left>",  shiftToPrev )
+            , ("M-S-<Right>", shiftToNext )
+
+            , ("M-S-q", spawn "gnome-session-save --gui --kill")
+            , ("M-S-l", spawn "gnome-screensaver-command -l")
+            ]
+
+
+-- vim: set sw=4 ts=4 et:
