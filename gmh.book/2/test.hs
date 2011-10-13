73ccf2221ce375b7fcc5695cc0a7b3732d66da2b
Satoru SATOH <satoru.satoh@gmail.com>
2011-10-14 03:53:37 +0900

    added tests

--- gmh.book/2/test.hs
+++ gmh.book/2/test.hs
@@ -1,12 +1,16 @@
 {-
 
-test.hs in chapter 2.
+test.hs in chapter 2. (p.15)
 
 -}
+module Double(double, quadruple, factorial, average) where
 
--- Double and Quadruple
+-- import Data.List
+import Test.QuickCheck
+import Test.HUnit
+
 double x = x + x
-quadruple x = double (double x)
+quadruple = double . double
 
 
 -- Factorial of a positive integer
@@ -14,3 +18,23 @@ factorial n = product [1..n]
 
 -- Average of a list of integers
 average ns = sum ns `div` length ns
+
+
+prop_double n = double n == n * 2
+        where types = n::Int
+
+prop_quadruple n = quadruple n == n * 4
+        where types = n::Int
+
+
+unittests = Test.HUnit.test [
+             "factorial 1" ~: factorial 1 ~?= 1
+            ,"factorial 3" ~: factorial 3 ~?= 6
+            ,"average [1]" ~: average [1] ~?= 1
+            ,"average [1, 2, 3]" ~: average [1, 2, 3] ~?= 2
+            ]
+
+runtests = do quickCheck prop_double
+              quickCheck prop_quadruple
+              runTestTT unittests
+
