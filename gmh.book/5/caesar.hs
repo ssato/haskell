bdf43437bf65a942d5cd005332dcbd19860b6594
Satoru SATOH <satoru.satoh@gmail.com>
2011-11-16 14:56:06 +0900

    added quickcheck test cases

--- gmh.book/5/caesar.hs
+++ gmh.book/5/caesar.hs
@@ -5,22 +5,12 @@ module Caesar where
 
 import Char
 import Data.List
+
 import Test.QuickCheck
 import Test.HUnit
 
--- tests
-tests = Test.HUnit.test ["let2int 'a'" ~: let2int 'a' ~?= 0,
-                         "let2int 'z'" ~: let2int 'z' ~?= 25,
-                         "int2let 0" ~: int2let 0 ~?= 'a',
-                         "int2let 25" ~: int2let 25 ~?= 'z',
-                         "shift 3 'a'" ~: shift 3 'a' ~?= 'd',
-                         "shift 3 'z'" ~: shift 3 'z' ~?= 'c',
-                         "shift -3 'c'" ~: shift (-3) 'c' ~?= 'z',
-                         "shift 3 ' '" ~: shift 3 ' ' ~?= ' ',
-                         "shift 3 'A'" ~: shift 3 'A' ~?= 'A',
-                         "encode 3 \"Haskell is fun\"" ~: encode 3 "Haskell is fun" ~?= "Hdvnhoo lv ixq",
-                         "encode (-3) \"Hdvnhoo lv ixq\"" ~: encode (-3) "Hdvnhoo lv ixq" ~?= "Haskell is fun",
-                         "rotate" ~: rotate 3 [1..5] ~?= [4,5,1,2,3] ]
+
+nlowers = length ['a'..'z']
 
 -- converts a lower-case letter between 'a' and 'z' into the corresponding
 -- integer between 0 and 25
@@ -33,7 +23,7 @@ int2let n = chr (ord 'a' + n)
 
 -- shift
 shift :: Int -> Char -> Char
-shift n c | Char.isLower c = int2let ((let2int c + n) `mod` 26) 
+shift n c | isLower c = int2let ((let2int c + n) `mod` nlowers)
           | otherwise      = c
 
 -- encode
@@ -51,9 +41,9 @@ percent n m = (fromIntegral n / fromIntegral m) * 100
 -- frequency table for any string
 freqs :: String -> [Float]
 freqs cs = [percent (count c cs) n | c <- ['a'..'z']]
-        where n = lowers cs
-              lowers cs = length [c | c <- cs, Char.isLower c]
-              count c cs = length [c' | c' <- cs, c == c']
+    where n = lowers cs
+          lowers cs = length [c | c <- cs, isLower c]
+          count c cs = length [c' | c' <- cs, c == c']
 
 -- chi-square statistic
 -- os : observed frequencies list, es : expected frequencies list
@@ -69,14 +59,45 @@ rotate n xs = drop n xs ++ take n xs
 -- utility functions 
 positions :: Eq a => a -> [a] -> [Int]
 positions x xs = [i | (x', i) <- zip xs [0..n], x == x']
-        where n = length xs - 1
+    where n = length xs - 1
 
 -- find most likely shift factor that was used to encode the string and try
 -- decoding the string
 crack :: String -> String
 crack cs = encode (-factor') cs
-        where factor' = head $ positions (minimum chitable) chitable
-              chitable = [chisqr (rotate n table') table | n <- [0..25]]
-              table' = freqs cs
+    where factor' = head $ positions (minimum chitable) chitable
+          chitable = [chisqr (rotate n table') table | n <- ns]
+          table' = freqs cs
+          ns = [0..(nlowers - 1)]
+
+-- tests
+unittests = Test.HUnit.test [
+     "let2int 'a'" ~: let2int 'a' ~?= 0
+    ,"let2int 'z'" ~: let2int 'z' ~?= 25
+    ,"int2let 0" ~: int2let 0 ~?= 'a'
+    ,"int2let 25" ~: int2let 25 ~?= 'z'
+    ,"shift 3 'a'" ~: shift 3 'a' ~?= 'd'
+    ,"shift 3 'z'" ~: shift 3 'z' ~?= 'c'
+    ,"shift -3 'c'" ~: shift (-3) 'c' ~?= 'z'
+    ,"shift 3 ' '" ~: shift 3 ' ' ~?= ' '
+    ,"shift 3 'A'" ~: shift 3 'A' ~?= 'A'
+    ,"encode 3 \"Haskell is fun\"" ~: encode 3 "Haskell is fun" ~?= "Hdvnhoo lv ixq"
+    ,"encode (-3) \"Hdvnhoo lv ixq\"" ~: encode (-3) "Hdvnhoo lv ixq" ~?= "Haskell is fun"
+    ,"rotate" ~: rotate 3 [1..5] ~?= [4,5,1,2,3]
+    ]
 
 
+isAlphaLower :: Char -> Bool
+isAlphaLower c = elem c ['a'..'z']
+
+prop_let2int_int2let c = isAlphaLower c ==> int2let (let2int c) == c
+    where types = c :: Char
+
+prop_shift n c = isAlphaLower c && n >= 0 ==> shift (- n) (shift n c) == c
+    where types = (c :: Char, n :: Int)
+
+runtests = do runTestTT unittests
+              quickCheckWith stdArgs {maxSuccess=20} prop_let2int_int2let
+              quickCheckWith stdArgs {maxSuccess=20} prop_shift
+
+-- vim:sw=4 ts=4 et:
