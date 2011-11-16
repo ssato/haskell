ca913136c90a97b2158d99cee6061025cd2356a0
Satoru SATOH <satoru.satoh@gmail.com>
2011-11-16 12:55:29 +0900

    updated

--- gmh.book/5/1.hs
+++ gmh.book/5/1.hs
@@ -1,20 +1,47 @@
 --
 -- 5.1 Generators
 --
-module Main where
+module GeneratorExamples where
+
 import Test.QuickCheck
+import Test.HUnit
+
 
 -- concatenates a list of lists
 concat' :: [[a]] -> [a]
 concat' xss = [x | xs <- xss, x <- xs]
 
 --  selects all the first components from a list of pairs
-firsts' :: [(a,b)] -> [a]
+firsts' :: [(a, b)] -> [a]
 firsts' ps = [x | (x, _) <- ps]
+
 
 -- calculates the length of a list
 --
--- length [] = 0
--- length x:xs = 1 + length xs
 length' :: [a] -> Int
 length' xs = sum [1 | _ <- xs]
+
+-- other solutions:
+length'' :: [a] -> Int
+length'' [] = 0
+length'' (x:xs) = 1 + length xs
+
+-- tests:
+unittests = Test.HUnit.test [
+     "concat' []" ~: (concat' [] :: [[Int]]) ~?= []
+    ,"concat' [[]]" ~: (concat' [[]] :: [[Int]]) ~?= []
+    ,"concat' [[1,2], [3], [4]]" ~: concat' [[1,2], [3], [4]] ~?= [1,2,3,4]
+    ,"firsts' []" ~: (firsts' [] :: [(Int, Char)]) ~?= []
+    ,"firsts' [(1, 'a'), (2, 'b')]" ~: firsts' [(1, 'a'), (2, 'b')] ~?= [1, 2]
+    ]
+
+prop_length' xs = length' xs == length xs
+    where types = xs :: [Int]
+
+prop_length'' xs = length'' xs == length xs
+    where types = xs :: [Int]
+
+runtests = runTestTT unittests >> quickCheck prop_length' >> quickCheck prop_length''
+
+
+-- vim:sw=4 ts=4 et:
--- gmh.book/5/2.hs
+++ gmh.book/5/2.hs
@@ -1,21 +1,54 @@
 --
 -- 5.2 Guards
 --
-module Main where
+module GuardExamples where
+
+import Test.QuickCheck
+import Test.HUnit
+
 
 -- list positive factors of a positive integer
 factors :: Int -> [Int]
 factors n = [x | x <- [1..n], n `mod` x == 0]
 
--- prime number
-prime :: Int -> Bool
-prime n = factors n == [1, n]
+-- other solutions:
+factors' :: Int -> [Int]
+factors' n = filter (\x -> n `mod` x == 0) [1..n]
 
--- prime numbers upto n
+-- prime number checker
+isPrime :: Int -> Bool
+isPrime n = factors n == [1, n]
+
+-- prime numbers from 2 upto n
 primes :: Int -> [Int]
-primes n = [x | x <- [2..n], prime x]
+primes n = [x | x <- [2..n], isPrime x]
 
+-- other solutions:
+primes' :: Int -> [Int]
+primes' n = filter isPrime [2..n]
+
+-- Hashmap, dict:
 -- returns the list of all values that are associated with a given key in a
 -- table which is a list of pairs comprising keys and values.
 find :: Eq a => a -> [(a,b)] -> [b]
 find k tbl = [v | (k', v) <- tbl, k == k']
+
+
+-- tests:
+prop_factors n = n > 0 && n < 1000 ==> null [1 | x <- xs, n `mod` x /= 0]
+    where types = n :: Int
+          xs = factors n
+
+prop_factors' n = n > 0 && n < 1000 ==> factors n == factors' n
+    where types = n :: Int
+
+unittests = Test.HUnit.test [
+     "primes 10" ~: primes 10 ~?= [2, 3, 5, 7]
+    ,"find 1 [(1, 'a'), (2, 'b'), (3, 'c'), (1, 'd')]" ~: find 1 [(1, 'a'), (2, 'b'), (3, 'c'), (1, 'd')] ~?= ['a', 'd']
+    ]
+
+runtests = do quickCheckWith stdArgs {maxSuccess=2} prop_factors
+              quickCheckWith stdArgs {maxSuccess=2} prop_factors'
+              runTestTT unittests
+
+-- vim:sw=4 ts=4 et:
