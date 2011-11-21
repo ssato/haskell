80fa6c4fc374ee6a105e342d18f119b866c50af0
Satoru SATOH <satoru.satoh@gmail.com>
2011-11-22 04:02:18 +0900

    finished up to 7.6

--- gmh.book/7/6.hs
+++ gmh.book/7/6.hs
@@ -1,93 +1,161 @@
---
--- 7.6 String transmitter
---
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Main where
+{-
+ - 7.6. String conversion: string <-> bit (int)
+ -}
 
-import Char
-import Test.QuickCheck hiding (test)
+module StringBinEncoder where
+
+
+import Data.Char
+
+import Test.QuickCheck
 import Test.HUnit
 
-unittests = Test.HUnit.test [
-        "bin2int" ~: bin2int [1,0,1,1] ~?= 13,
-        "bin2int'" ~: bin2int' [1,0,1,1] ~?= 13,
-        "int2bin'" ~: int2bin 13 ~?= [1,0,1,1],
-        "make8" ~: make8 [1,0,1,1] ~?= [1,0,1,1,0,0,0,0],
-        "encode" ~: encode "abc" ~?= [1,0,0,0,0,1,1,0,0,1,0,0,0,1,1,0,1,1,0,0,0,1,1,0],
-        "decode" ~: decode [1,0,0,0,0,1,1,0,0,1,0,0,0,1,1,0,1,1,0,0,0,1,1,0] ~?= "abc",
-        "transmit" ~: transmit "higher-order functions are easy" ~?= "higher-order functions are easy"
-        ]
 
--- Bit
--- FIXME:
 {-
-newtype Bitnumber = Bitnumber Int deriving (Eq, Ord, Show, Num)
-instance Arbitrary Bitnumber where
-        arbitrary = oneof [0, 1]
+
+decimal:
+
+234 = (2 * 10 ^ 2) + (3 * 10 ^ 1) + (4 * 10 ^ 0)
+    = (4 * 10 ^ 0) + (3 * 10 ^ 1) + (2 * 10 ^ 2)
+    = 4 + 10 * (3 + 10 * 2)
+
+binary:
+
+1101 = (2 ^ 3 * 1) + (2 ^ 2 * 1) + (2 ^ 1 * 1) + (2 ^ 0 * 1)
+     = (2 ^ 0 * 1) + (2 ^ 1 * 1) + (2 ^ 2 * 1) + (2 ^ 3 * 1)
+     = 1 + 2 * (1 + 2 * (1 + 2 * 1))
+
+-}
+type Bit = Int  -- Synonim
 
 
-prop_bin2int' bs = bin2int' bs == bin2int bs
-        where types = bs::[Bitnumber]
+-- binary -> int
+bin2int :: [Bit] -> Int
+bin2int bits = sum [w * b | (w, b) <- zip weights bits]
+    where weights = iterate' (* 2) 1
 
-prop_encode_decode cs = decode (encode cs) == cs
-        where types = cs::[Char]
+iterate' :: (a -> a) -> a -> [a]
+iterate' f x = x : iterate' f (f x)
 
-prop_transmit s = transmit s == s
-        where types = s::String
+-- other solution
+bin2int' :: [Bit] -> Int
+bin2int' = foldr (\x -> \acc -> x + 2 * acc) 0
+
+
+unittests_bin2int = Test.HUnit.test [
+     "bin2int [1, 1, 0, 1]" ~: bin2int [1, 1, 0, 1] ~?= 11
+    ,"bin2int' [1, 1, 0, 1]" ~: bin2int' [1, 1, 0, 1] ~?= 11
+    ]
+
+{-
+
+int -> binary:
+
+
+13 ==> [13 `mod` 2, (13 `div` 2) `mod` 2, (13 `div` 2 `div` 2) `mod` 2, (13 `div` 2 `div` 2 `div` 2) `mod` 2]
+
 -}
 
-runtests = do runTestTT unittests
-              -- quickCheck prop_encode_decode
-              -- quickCheck prop_transmit
-
-
--- Base conversion
-type Bit = Int
-
--- convert binary representation to int
--- 1. naive implementation using list comprehension
-bin2int :: [Bit] -> Int
-bin2int bits = sum [w * b | (w,b) <- zip weights bits]
-        where weights = iterate (* 2) 1
-
--- 2. another version
-bin2int' :: [Bit] -> Int 
-bin2int' = foldr (\x y -> x + 2 * y) 0
-
--- convert int to binary representation
 int2bin :: Int -> [Bit]
 int2bin 0 = []
-int2bin n = n `mod` 2 : int2bin (n `div` 2)
+int2bin n = m : int2bin d
+    where m = n `mod` 2
+          d = n `div` 2
 
--- truncates or exnteds a binary number as appropriate to make it precisely
--- eight bits
+
+unittests_int2bin = Test.HUnit.test [
+     "int2bin 11" ~: int2bin 11 ~?= [1, 1, 0, 1]
+    ]
+
+
+-- normalize to 8 bit:
+-- (only 8 bit chars are processed)
+--
 make8 :: [Bit] -> [Bit]
-make8 bits = take 8 (bits ++ repeat 0)
+make8 bits = take 8 $ bits ++ repeat 0
+
+unittests_make8 = Test.HUnit.test [
+     "make8 [1, 1, 0, 1]" ~: make8 [1, 1, 0, 1] ~?= [1, 1, 0, 1, 0, 0, 0, 0]
+    ,"make8 [1, 1, 0, 1, 0, 0, 0, 0, 1, 1]" ~: make8 [1, 1, 0, 1, 0, 0, 0, 0, 1, 1] ~?= [1, 1, 0, 1, 0, 0, 0, 0]
+    ]
 
 
--- Transmission
--- encodes a string of characters as a list of bits by converting each
--- character into a Unicode number, converting each such number into an 8-bit
--- binary number, and concatenating each of these numbers together to produce a
--- list of bits.
+-- Encode: String -> [Bit] (8 bits)
+--
+-- String ==> [Unicode codepoint]  (w/ ord)
+--        ==> [8 bits binary representation]  (w/ make8 . int2bin)
+{-
+encode cs = concat [ c |c <- cs]
+    where f c = make8 $ int2bin $ ord c
+
+f ==> make8 . int2bin . ord
+-}
+
 encode :: String -> [Bit]
-encode = concat . map (make8 . int2bin . Char.ord)
+encode = concat . map (make8 . int2bin . ord)
 
--- chops such a list up into 8-bit binary numbers
+unittests_encode = Test.HUnit.test [
+     "encode \"abc\"" ~: encode "abc" ~?= [1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0]
+    ]
+
+
+-- Decode: [Bit] -> original string
+--
+-- [Bit] ==> [[Bit]]   (split per each 8 bits)
+--       ==> [[Int]]   (w/ bit2int)
+--       ==> [[Char]]  (w/ chr :: Int -> Char)
+--
 chop8 :: [Bit] -> [[Bit]]
-chop8 []   = []
+chop8 [] = []
 chop8 bits = take 8 bits : chop8 (drop 8 bits)
 
--- decode
+-- decode bits = [chr (bin2int bs) | bs <- chop8 bits]
+--             ==> map (chr . bin2int) (chop8 bits)
 decode :: [Bit] -> String
 decode = map (chr . bin2int) . chop8
 
--- simulates the transmission of a string of characters as a list of bits
+unittests_decode = Test.HUnit.test [
+     "decode [1, 0, ..., 1, 1]" ~: decode [1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0] ~?= "abc"
+    ]
+
+prop_encode_decode s = decode (encode s) == s
+    where types = s :: String
+
+
+
+-- transmit
 transmit :: String -> String
 transmit = decode . channel . encode
 
 channel :: [Bit] -> [Bit]
-channel = id
+channel = id   -- no errors (no change, no noise, ...)
+
+
+runtests = do runTestTT unittests_bin2int
+              runTestTT unittests_int2bin
+              runTestTT unittests_make8
+              runTestTT unittests_encode
+              runTestTT unittests_decode
+              quickCheck prop_encode_decode
 
 
 
+{- session log:
+
+ghci> :l 7/6.hs
+[1 of 1] Compiling StringBinEncoder ( 7/6.hs, interpreted )
+Ok, modules loaded: StringBinEncoder.
+ghci> runtests
+Cases: 2  Tried: 2  Errors: 0  Failures: 0
+Cases: 1  Tried: 1  Errors: 0  Failures: 0
+Cases: 2  Tried: 2  Errors: 0  Failures: 0
+Cases: 1  Tried: 1  Errors: 0  Failures: 0
+Cases: 1  Tried: 1  Errors: 0  Failures: 0
++++ OK, passed 100 tests.
+ghci>
+
+-}
+
+
+-- vim:sw=4 ts=4 et:
